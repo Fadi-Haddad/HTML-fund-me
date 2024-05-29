a89716bedead49c80746ebb07e38d6e82ab0ec15
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-05-29 23:57:04 +0300

    Fix module import and define buttons in index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import { ethers } from "./ethers-5.2.esm.min"
+import { ethers } from "./ethers-5.2.esm.min.js"
+
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+
+connectButton.onclick= connect
+fundButton.onclick= fund
 
 async function connect(){
     window.ethereum
@@ -6,7 +13,13 @@ async function connect(){
     : alert("Metamask not installed!!")
 }
 async function fund(){
-    window.ethereum
-    ? alert("funding")
-    : alert("Metamask not installed!!")
+    // we will need provider // connection to the blockchain
+    // signer // wallet //someone with enough gas
+    // contract to interact with (ABI and addresses)
+
+    if(window.ethereum){
+        const provider = new ethers.providers.Web3Provider(Window.ethereum) // establish a connection to the blockchain
+        const signer  = provider.getSigner() // returns the connected account in metamask
+        console.log(signer)
+    }
 }
