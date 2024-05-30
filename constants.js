0b8c1952b70044043232544a899532071653eb8b
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-05-30 22:10:01 +0300

    Finish fund function to send money

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-export const cotractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 export const abi = [
     {
       "inputs": [
@@ -128,4 +128,4 @@ export const abi = [
       "stateMutability": "payable",
       "type": "receive"
     }
-  ],
+  ]
--- index.js
+++ index.js
@@ -4,8 +4,12 @@ import { ethers } from "./ethers-5.2.esm.min.js"
 const connectButton = document.getElementById("connectButton")
 const fundButton = document.getElementById("fundButton")
 
+import { abi, contractAddress } from "./constants.js"
+
 connectButton.onclick= connect
 fundButton.onclick= fund
+
+
 
 async function connect(){
     window.ethereum
@@ -13,13 +17,15 @@ async function connect(){
     : alert("Metamask not installed!!")
 }
 async function fund(){
+    const sentAmount = "1"
     // we will need provider // connection to the blockchain
     // signer // wallet //someone with enough gas
     // contract to interact with (ABI and addresses)
 
     if(window.ethereum){
-        const provider = new ethers.providers.Web3Provider(Window.ethereum) // establish a connection to the blockchain
+        const provider = new ethers.providers.Web3Provider(window.ethereum) // establishes a connection to the blockchain
         const signer  = provider.getSigner() // returns the connected account in metamask
-        console.log(signer)
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        const transactionResponse = await contract.fund({value: ethers.utils.parseEther(sentAmount)})
     }
 }
