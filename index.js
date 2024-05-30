5bd62a3262a6ae8ef681bd3623c2ebb4e9da1e15
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-05-30 22:46:44 +0300

    Wrap money transfer code by a try catch block to account for transaction rejection by user

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-import { ethers } from "./ethers-5.2.esm.min.js"
+import { ethers } from "./ethers-5.2.esm.min.js";
 
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
 
-const connectButton = document.getElementById("connectButton")
-const fundButton = document.getElementById("fundButton")
+import { abi, contractAddress } from "./constants.js";
 
-import { abi, contractAddress } from "./constants.js"
+connectButton.onclick = connect;
+fundButton.onclick = fund;
 
-connectButton.onclick= connect
-fundButton.onclick= fund
-
-
-
-async function connect(){
-    window.ethereum
-    ? await window.ethereum.request({method: "eth_requestAccounts"})
-    : alert("Metamask not installed!!")
+async function connect() {
+  window.ethereum
+    ? await window.ethereum.request({ method: "eth_requestAccounts" })
+    : alert("Metamask not installed!!");
 }
-async function fund(){
-    const sentAmount = "1"
-    // we will need provider // connection to the blockchain
-    // signer // wallet //someone with enough gas
-    // contract to interact with (ABI and addresses)
+async function fund() {
+  const sentAmount = "1";
+  // we will need provider // connection to the blockchain
+  // signer // wallet //someone with enough gas
+  // contract to interact with (ABI and addresses)
 
-    if(window.ethereum){
-        const provider = new ethers.providers.Web3Provider(window.ethereum) // establishes a connection to the blockchain
-        const signer  = provider.getSigner() // returns the connected account in metamask
-        const contract = new ethers.Contract(contractAddress, abi, signer)
-        const transactionResponse = await contract.fund({value: ethers.utils.parseEther(sentAmount)})
+  if (window.ethereum) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum); // establishes a connection to the blockchain
+    const signer = provider.getSigner(); // returns the connected account in metamask
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponse = await contract.fund({value: ethers.utils.parseEther(sentAmount),});
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
